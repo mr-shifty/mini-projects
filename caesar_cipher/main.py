34c9cedf812711ba51d0ec3206bb78cc46c5c8ed
Pavel Melnikov <room.apps@mail.ru>
2023-05-28 19:21:16 +0300

    Added upper chars.

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -4,21 +4,33 @@ class CaesarCiepher:
         self.encrypted_message = ""
 
     def ru_encrypt(self, message):
-        self.first_chr = 1072
+        self.lower_first_chr = 1072
+        self.upper_first_chr = 1040
         self.quantity_chrs = 32
         for chars in message:
             if chars.isalpha():
-                encoded_char = chr((ord(chars) - self.first_chr + self.shift)
-                                   % self.quantity_chrs + self.first_chr)
-                self.encrypted_message += encoded_char
+                if chars.islower():
+                    encoded_char = chr((ord(chars) - self.lower_first_chr
+                                        + self.shift)
+                                       % self.quantity_chrs
+                                       + self.lower_first_chr
+                                       )
+                    self.encrypted_message += encoded_char
+                elif chars.isupper():
+                    encoded_char = chr((ord(chars) - self.upper_first_chr
+                                        + self.shift)
+                                       % self.quantity_chrs
+                                       + self.upper_first_chr
+                                       )
+                    self.encrypted_message += encoded_char
             else:
                 self.encrypted_message += chars
         return self.encrypted_message
 
 
 def main():
-    message = "блажен, кто верует, тепло ему на свете!"
-    shift = int(input())
+    message = "Блажен, кто верует, тепло ему на свете!"
+    shift = 10
     ciepher = CaesarCiepher(shift)
     encripted_message = ciepher.ru_encrypt(message)
     print(encripted_message)
