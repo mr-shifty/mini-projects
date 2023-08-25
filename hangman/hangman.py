43e0f35b4e314f9bc6b64e5d20691e9993cf2a4e
Pavel Melnikov <room.apps@mail.ru>
2023-08-25 12:44:34 +0300

    Added statistic and fix bugs

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,18 +1,26 @@
 from checks import check_letter, is_valid, is_valid_answer
 from utils import display_hangman, get_word
-from colorama import init, Fore, Style, Back
+from colorama import init, Fore, Style
 
 init()
 
+count_victory: int = 0
+count_defeat: int = 0
 
-def play(word):
+
+def play(word, name):
     """Основная логика программы."""
 
-    word_completion = '_ ' * len(word)
-    guessed = False
-    guessed_letters = []
+    global count_victory
+    global count_defeat
+
+    word_completion: str = '_ ' * len(word)
+    guessed: bool = False
+    guessed_letters: list = []
     # guessed_word = []
     tries = 6
+    attempt = 1
+
     print(f'{word}')
     print(f'{Fore.GREEN}{Style.BRIGHT}{display_hangman(tries)}\n\n{Style.RESET_ALL}'
           f'{Fore.BLUE}Я загадал слово, нужно его угадать{Style.RESET_ALL}\n{word_completion}\n'
@@ -32,7 +40,8 @@ def play(word):
             if len(input_letter) > 1:
                 if input_letter == word:
                     print(f"{Fore.GREEN}{Style.BRIGHT}\n{' ' * 20}Угадал слово, молодец 🥳{Style.RESET_ALL}")
-                    play_again()
+                    count_victory += 1
+                    play_again(name)
 
             if input_letter in word:
                 guessed_letters.append(input_letter)
@@ -46,8 +55,10 @@ def play(word):
 
                 if guessed:
                     check_letter(word, guessed_letters)
-                    print(f'{Fore.GREEN}{Style.BRIGHT}\n\n{" " * 25}Ты победил, красавец🎉🎊\n{Style.RESET_ALL}')
-                    play_again()
+                    print(f'{Fore.GREEN}{Style.BRIGHT}\n\n{" " * 25}Ты победил, красавец 🎉🎊\n{Style.RESET_ALL}')
+                    print(f'{Fore.GREEN}{" " * 25}Угадал(а) с {Fore.YELLOW}{attempt} {Fore.GREEN}попытки из {Fore.YELLOW}6')
+                    count_victory += 1
+                    play_again(name)
 
             else:
                 if tries > 1:
@@ -56,6 +67,7 @@ def play(word):
                         f'{Fore.YELLOW}{Style.BRIGHT}{display_hangman(tries)}{Style.RESET_ALL}')
                     print(f"{Fore.RED}\n{' ' * 25}Не угадал, попробуй еще{Style.RESET_ALL}\n")
                     check_letter(word, guessed_letters)
+                    attempt += 1
                 else:
                     tries -= 1
                     print(
@@ -63,26 +75,33 @@ def play(word):
                     )
                     print(f"{Fore.RED}{Style.BRIGHT} {' ' * 10}Ты проиграл. К сожелению, без жертв не обошлось\n\n{Style.RESET_ALL}")
                     print(f'{Fore.BLUE}Загаданное слово: {Fore.LIGHTMAGENTA_EX}{word}{Style.RESET_ALL}')
-                    play_again()
+                    count_defeat += 1
+                    play_again(name)
 
 
-def play_again():
+def play_again(name):
     """Отвечает за повтор игры."""
+
+    print(f'\n{Fore.BLUE}Твоя статистика, {name}:\n\n'
+          f'{Fore.GREEN}Победы: {Fore.YELLOW}{count_victory}\n'
+          f'{Fore.RED}Поражения: {Fore.YELLOW}{count_defeat}\n{Style.RESET_ALL}')
 
     answer = input(f'\n{Fore.LIGHTGREEN_EX}Хотите сыграть еще?{Fore.YELLOW} д/н\n\n{Fore.CYAN}Ваш ответ: {Style.RESET_ALL}').lower()
     if is_valid_answer(answer):
-        play_again_answer(answer)
+        play_again_answer(answer, name)
     else:
         while is_valid_answer(answer) is False:
             answer = input(
                 f'\n{Fore.RED}Введите корректный ответ {Fore.YELLOW}д/н\n\n{Fore.CYAN}Ваш ответ: {Style.RESET_ALL}').lower()
             if is_valid_answer(answer):
-                play_again_answer(answer)
+                play_again_answer(answer, name)
 
 
-def play_again_answer(answer: str) -> str:
+def play_again_answer(answer: str, name: str) -> str:
+    """Варианты ответов для повтора игры."""
+
     if answer == 'д':
-        return play(get_word().upper())
+        return play(get_word().upper(), name)
     elif answer == 'н':
         print(f"\n{Fore.MAGENTA}Заходите снова, вам обязательно повезет 😌\n{Style.RESET_ALL}")
         exit()
@@ -90,13 +109,14 @@ def play_again_answer(answer: str) -> str:
 
 def main():
     """Главная исполняющая функция."""
-    name: str = input(f"\n{Fore.MAGENTA}Привет, как тебя зовут?\n\n{Fore.CYAN}Ваше имя: {Style.RESET_ALL}").capitalize()
+
+    name: str = input(f"\n{Fore.MAGENTA}Привет, как тебя зовут?\n\n{Fore.CYAN}Ваше имя: {Style.RESET_ALL}").title()
     print(
         f'{Fore.BLUE}\nПривет, {name}. Давай сыграем с тобой в виселицу.\n'
         f'\n{Fore.MAGENTA}Не переживай, никто не умрет,'
         f'{Style.BRIGHT}но это не точно 😈\n\n{Style.RESET_ALL}'
     )
-    print(play(get_word().upper()))
+    print(play(get_word().upper(), name))
 
 
 if __name__ == '__main__':
